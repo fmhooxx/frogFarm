d668ebb57061773e39aa5e24cdf7b4ff0d72b626
fmhooxx <1321058503@qq.com>
2019-12-14 18:07:07 +0800

    久航康品首页一级页面完成 2019/12/14

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // 首页轮播图数据
+    swiperList: [{
+        id: 0,
+        url: '../../images/index-swiper.png'
+      },
+      {
+        id: 1,
+        url: '../../images/index-swiper.png'
+      },
+      {
+        id: 2,
+        url: '../../images/index-swiper.png'
+      }
+    ],
+    // 商品详情数据
+    shopList: [{
+        id: 0,
+        url: '../../images/index-recommend-details.jpg',
+        member: '铂金会员',
+        type: '套餐A',
+        num: '九子九仁汤5盒',
+        price: '299',
+        used: '599.00'
+      },
+      {
+        id: 1,
+        url: '../../images/index-recommend-details.jpg',
+        member: '普通会员',
+        type: '套餐B',
+        num: '九子九仁汤5盒',
+        price: '2999',
+        used: '5999.00'
+      },
+      {
+        id: 2,
+        url: '../../images/index-recommend-details.jpg',
+        type: '套餐A',
+        num: '九子九仁汤5盒',
+        price: '399',
+        used: '699.00'
+      },
+      {
+        id: 3,
+        url: '../../images/index-recommend-details.jpg',
+        type: '套餐B',
+        num: '九子九仁汤5盒',
+        price: '299',
+        used: '599.00'
+      }
+    ],
+    // 商品可以滑动区域的数据
+    infoList: [
+      {
+        id: 0,
+        url: '../../images/index-shop-service.png',
+        service: '特色采耳',
+        timer: '30分钟',
+        price: '41.8',
+        used: '¥39.9'
+      },
+      {
+        id: 1,
+        url: '../../images/index-shop-service.png',
+        service: '特色采耳',
+        timer: '30分钟',
+        price: '41.8',
+        used: '¥39.9'
+      },
+      {
+        id: 2,
+        url: '../../images/index-shop-service.png',
+        service: '特色采耳',
+        timer: '30分钟',
+        price: '41.8',
+        used: '¥39.9'
+      },
+      {
+        id: 3,
+        url: '../../images/index-shop-service.png',
+        service: '特色采耳',
+        timer: '30分钟',
+        price: '41.8',
+        used: '¥39.9'
+      }
+    ]
+  },
+})
